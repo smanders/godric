1e75c47cda6ec3f07317a23ac12fa86f80fa0024
Scott M Anderson <smanders.sdl@gmail.com>
2020-11-28 10:59:48 -0700

    test.sh: generate random test files

--- test/test.sh
+++ test/test.sh
@@ -1,26 +1,47 @@
-mkdir input
+mkdir -p input
 cd input
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2017_2017 ENGAGEMENT LETTER"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2017_2017 Tax Documents"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2016_2016 ENGAGEMENT LETTER"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2016_2016 Tax Documents"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2018_2018 ENGAGEMENT LETTER"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "ADAMSJAL_ADAMS, JAMES L. & LUVERNE_UltraTax CS 12-31-2019_2019 Tax Documents"
-touch "AHERNCAR_AHERN, CARRIE A._UltraTax CS 12-31-2017_2017 ENGAGEMENT LETTER"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2017_2017 Tax Documents"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2016_2016 ENGAGEMENT LETTER"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2016_2016 Tax Documents"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2018_2018 ENGAGEMENT LETTER"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "AHERNCAR_AHERN, CARRIE A_UltraTax CS 12-31-2019_2019 Tax Documents"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2017_2017 ENGAGEMENT LETTER"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2017_2017 Tax Documents"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2016_2016 ENGAGEMENT LETTER"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2016_2016 Tax Documents"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2018_2018 ENGAGEMENT LETTER"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2018_2018 Tax Documents"
-touch "ANDERDAL_ANDERSON, RUTH J._UltraTax CS 12-31-2019_2019 Tax Documents"
+url=https://www2.census.gov/topics/genealogy/1990surnames
+[ ! -f names_female.txt ] && wget -qO- ${url}/dist.female.first | awk '{print $1}' > names_female.txt
+[ ! -f names_male.txt ] && wget -qO- ${url}/dist.male.first | awk '{print $1}' > names_male.txt
+[ ! -f names_last.txt ] && wget -qO- ${url}/dist.all.last | awk '{print $1}' > names_last.txt
+function getName()
+{
+  female=`sort --random-sort names_female.txt | head -n 1`
+  male=`sort --random-sort names_male.txt | head -n 1`
+  last=`sort --random-sort names_last.txt | head -n 1`
+  fini="$(echo ${female} | head -c 1)"
+  mini="$(echo ${male} | head -c 1)"
+  local name="${last}${mini}${fini}_${last}, ${male} & ${female}"
+  echo "$name"
+}
+field1=(\
+  "_UltraTax CS 12-31-"\
+  )
+field3=(\
+  " ENGAGEMENT LETTER"\
+  " Tax Documents"\
+  " Tax Exemptions"\
+  " Form 1040"\
+  " Form 1040EZ"\
+  " Form 1040A"\
+  " Form W-2"\
+  " Form W-4"\
+  " Form W-4P"\
+  " Form 1099-MISC"\
+  " Form 1098"\
+  )
+names=100
+until [ $names -lt 1 ]; do
+  f0=$(getName)
+  for f1 in "${field1[@]}"; do
+    year=1990
+    until [ $year -gt 2019 ]; do
+      f2="${year}_${year}"
+      for f3 in "${field3[@]}"; do
+        touch "${f0}${f1}${f2}${f3}"
+      done
+      ((year++))
+    done
+  done
+  ((names--))
+done
